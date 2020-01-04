819e94d098fcfb36061e9fcd3139e522c3c544de
tmayhew <57470309+tmayhew@users.noreply.github.com>
2020-01-04 13:35:21 -0800

    Update cbb.R

--- cbb.R
+++ cbb.R
@@ -6,6 +6,7 @@ library(brnn)
 
 # college basketball stats: by year
 # team stats
+given_year = 2019
 scrape_cbbteams <- function(given_year = 2019){
   year = paste(given_year)
   link <- paste0("https://www.sports-reference.com/cbb/seasons/",year,"-school-stats.html")
@@ -27,11 +28,28 @@ scrape_cbbteams <- function(given_year = 2019){
   teams <- na.omit(teams)
   return(teams)
 }
-stats2020 <- scrape_cbbteams(2020)
-stats2019 <- scrape_cbbteams(2019)
-stats2018 <- scrape_cbbteams(2018)
-stats2017 <- scrape_cbbteams(2017)
+mar.mad <- function(df = stats2019){
+  for(i in 1:nrow(df)){
+    split <- strsplit(df$Tm[i], split = "")[[1]]
+    ncaa.c <- paste(split[c(length(split)-3,length(split)-2,length(split)-1,length(split))], collapse = "")
+    if(ncaa.c == "NCAA"){
+      df$marmad[i] <- 1
+    } else{
+      df$marmad[i] <- 0
+    }
+  }
+  return(df)
+}
 
-split <- strsplit(stats2019$Tm[1], split = 
-           "")[[1]]
-ncaa.c <- merge(split[c(length(split)-3,length(split)-2,length(split)-1,length(split))])
+stats2020 <- scrape_cbbteams(2020)
+stats2019 <- mar.mad(scrape_cbbteams(2019))
+stats2018 <- mar.mad(scrape_cbbteams(2018))
+stats2017 <- mar.mad(scrape_cbbteams(2017))
+stats2016 <- mar.mad(scrape_cbbteams(2016))
+stats2015 <- mar.mad(scrape_cbbteams(2015))
+stats2014 <- mar.mad(scrape_cbbteams(2014))
+stats2013 <- mar.mad(scrape_cbbteams(2013))
+stats2012 <- mar.mad(scrape_cbbteams(2012))
+stats2011 <- mar.mad(scrape_cbbteams(2011))
+stats2010 <- mar.mad(scrape_cbbteams(2010))
+
